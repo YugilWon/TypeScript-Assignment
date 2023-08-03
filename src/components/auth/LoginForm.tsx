c9c3fcb5a576de6bd0d4fdd7060e811737b81891
YuGil <yugil3150@naver.com>
2023-08-03 14:07:07 +0900

    first commit

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,18 +1,40 @@
 import React from "react";
 import { Form, Input, Button } from "antd";
 import styled from "styled-components";
+import axios from "axios";
+import shortid from "shortid";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm: React.FC = () => {
+  const navigate = useNavigate();
   const handleLogin = async (values: any) => {
-    alert("TODO 요구사항에 맞추어 기능을 완성해주세요.");
-
-    // TODO: email과 password를 DB에서 찾아서 로그인 검증
-    // TODO: 일치하는 유저가 없는 경우 "일치하는 유저를 찾을 수 없습니다." alert
-    // TODO: 네트워크 등 기타 문제인 경우, "일시적인 오류가 발생하였습니다. 고객센터로 연락주세요." alert
-    // TODO: 성공 시(1), "로그인에 성공하였습니다. 메인 페이지로 이동합니다." alert
-    // TODO: 성공 시(2), localStorage에 token과 email을 저장
+    const { email, password } = values;
     // TODO: 성공 시(3), token은 shortId로 생성
-    // TODO: 성공 시(4), "/" 라우터로 이동
+    const token = shortid();
+
+    try {
+      // TODO: email과 password를 DB에서 찾아서 로그인 검증
+      const response = await axios.get(
+        `http://localhost:4000/users?email=${values.email}${values.password}`
+      );
+      // TODO: 일치하는 유저가 없는 경우 "일치하는 유저를 찾을 수 없습니다." alert
+      if (response.data.length < 1) {
+        alert("일치하는 유저를 찾을 수 없습니다.");
+        return false;
+      }
+      // TODO: 성공 시(1), "로그인에 성공하였습니다. 메인 페이지로 이동합니다." alert
+      alert("로그인에 성공했습니다. 메인 페이지로 이동합니다.");
+
+      // TODO: 성공 시(2), localStorage에 token과 email을 저장
+      localStorage.setItem("email", email);
+      localStorage.setItem("token", token);
+
+      // TODO: 성공 시(4), "/" 라우터로 이동
+      navigate("/");
+    } catch (err) {
+      // TODO: 네트워크 등 기타 문제인 경우, "일시적인 오류가 발생하였습니다. 고객센터로 연락주세요." alert
+      alert("일시적인 오류가 발생하였습니다. 고객센터로 연락주세요.");
+    }
   };
 
   return (
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,35 +1,72 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
 import { Button, Input } from "antd";
+import { useCookies } from "react-cookie";
 
 const Main: React.FC<any> = () => {
   const [data, setData] = useState([]);
   const [contents, setContents] = useState<string>("");
+  const [cookies, setCookie] = useCookies<string>(["email"]);
 
-  const fetchData = () => {
-    alert("TODO 요구사항에 맞추어 기능을 완성해주세요.");
-
+  const fetchData = async () => {
     // TODO: 데이터베이스에서 boards 리스트 가져오기
-    // TODO: 가져온 결과 배열을 data state에 set 하기
-    // TODO: 네트워크 등 기타 문제인 경우, "일시적인 오류가 발생하였습니다. 고객센터로 연락주세요." alert
+    try {
+      const response = await axios.get("http://localhost:4000/boards");
+      const data = response.data;
+      // TODO: 가져온 결과 배열을 data state에 set 하기
+      setData(data);
+    } catch (error) {
+      // TODO: 네트워크 등 기타 문제인 경우, "일시적인 오류가 발생하였습니다. 고객센터로 연락주세요." alert
+      console.error("Error fetching boards:", error);
+      alert("일시적인 오류가 발생하였습니다. 고객센터로 연락주세요.");
+    }
   };
 
   useEffect(() => {
     // TODO: 해당 useEffect는 최초 마운트시에만 동작하게 제어
     fetchData();
-  });
+  }, []);
 
-  const handleBoardSubmit = (e: any) => {
-    alert("TODO 요구사항에 맞추어 기능을 완성해주세요.");
-
+  const handleBoardSubmit = async (e: React.FormEvent) => {
     // TODO: 자동 새로고침 방지
+    e.preventDefault();
+
     // TODO: 이메일과 contents를 이용하여 post 요청 등록(isDeleted 기본값은 false)
-    // TODO: 네트워크 등 기타 문제인 경우, "일시적인 오류가 발생하였습니다. 고객센터로 연락주세요." alert
-    // TODO: 성공한 경우, "작성이 완료되었습니다. 아직 자동 새로고침이 불가하여 수동으로 갱신합니다." alert
-    // TODO: 처리완료 후, reload를 이용하여 새로고침
+    const newPost = {
+      email: cookies.email,
+      contents,
+      isDeleted: false,
+    };
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/boards",
+        newPost,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.status === 200 || response.status === 201) {
+        // TODO: 성공한 경우, "작성이 완료되었습니다. 아직 자동 새로고침이 불가하여 수동으로 갱신합니다." alert
+        alert(
+          "작성이 완료되었습니다. 아직 자동 새로고침이 불가하여 수동으로 갱신합니다."
+        );
+        // TODO: 처리완료 후, reload를 이용하여 새로고침
+        window.location.reload();
+      } else {
+        throw new Error("Network response was not ok");
+      }
+    } catch (error) {
+      // TODO: 네트워크 등 기타 문제인 경우, "일시적인 오류가 발생하였습니다. 고객센터로 연락주세요." alert
+      console.error("Error adding board:", error);
+      alert("일시적인 오류가 발생하였습니다. 고객센터로 연락주세요.");
+    }
   };
 
-  const handleInputChange = (e: any) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setContents(e.target.value);
   };
 
@@ -50,7 +87,7 @@ const Main: React.FC<any> = () => {
               {index + 1}. {item.contents}
             </span>
             {/* // TODO: 로그인 한 user의 이메일과 일치하는 경우에만 삭제버튼 보이도록 제어 */}
-            <Button>삭제</Button>
+            {cookies.email === item.email && <Button>삭제</Button>}
           </ListItem>
         ))}
       </ListWrapper>
