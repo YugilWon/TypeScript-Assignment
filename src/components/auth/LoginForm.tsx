00c4af6b75560b97792f02d5f7d8519c1e762782
YuGil <yugil3150@naver.com>
2023-08-03 21:22:57 +0900

    Remove LocalStorage Token & Email

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Form, Input, Button } from "antd";
 import styled from "styled-components";
 import axios from "axios";
@@ -9,28 +9,35 @@ const LoginForm: React.FC = () => {
   const navigate = useNavigate();
   const handleLogin = async (values: any) => {
     const { email, password } = values;
+    console.log(values.email);
+    console.log(values.password);
     // TODO: 성공 시(3), token은 shortId로 생성
     const token = shortid();
 
     try {
       // TODO: email과 password를 DB에서 찾아서 로그인 검증
       const response = await axios.get(
-        `http://localhost:4000/users?email=${values.email}${values.password}`
+        `http://localhost:4000/users?email=${values.email}&password=${values.password}`
       );
+
       // TODO: 일치하는 유저가 없는 경우 "일치하는 유저를 찾을 수 없습니다." alert
-      if (response.data.length < 1) {
+      if (response.data.length <= 1) {
         alert("일치하는 유저를 찾을 수 없습니다.");
         return false;
+      } else {
+        // TODO: 성공 시(1), "로그인에 성공하였습니다. 메인 페이지로 이동합니다." alert
+        alert("로그인에 성공했습니다. 메인 페이지로 이동합니다.");
+        // TODO: localStorage에 있는 token, email을 제거
+        localStorage.removeItem("email");
+        localStorage.removeItem("token");
+
+        // TODO: 성공 시(2), localStorage에 token과 email을 저장
+        localStorage.setItem("email", email);
+        localStorage.setItem("token", token);
+
+        // TODO: 성공 시(4), "/" 라우터로 이동
+        navigate("/");
       }
-      // TODO: 성공 시(1), "로그인에 성공하였습니다. 메인 페이지로 이동합니다." alert
-      alert("로그인에 성공했습니다. 메인 페이지로 이동합니다.");
-
-      // TODO: 성공 시(2), localStorage에 token과 email을 저장
-      localStorage.setItem("email", email);
-      localStorage.setItem("token", token);
-
-      // TODO: 성공 시(4), "/" 라우터로 이동
-      navigate("/");
     } catch (err) {
       // TODO: 네트워크 등 기타 문제인 경우, "일시적인 오류가 발생하였습니다. 고객센터로 연락주세요." alert
       alert("일시적인 오류가 발생하였습니다. 고객센터로 연락주세요.");
